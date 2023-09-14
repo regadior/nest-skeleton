2bbe5b42d43353844f425d957664041c73fc6f69
regadior <regadior@gmail.com>
2023-09-14 15:56:59 +0200

    feat: 🎸 Minor changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { ConfigModule } from '@nestjs/config';
+import { LoggerModule } from "nestjs-pino";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    LoggerModule.forRoot({
+      pinoHttp: {
+        transport: {
+          target: 'pino-pretty',
+        },
+      },
+    }),
+  ],
+  // controllers: [HealthController],
+  // providers: [HealthCheckerUseCase],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { Logger, LoggerErrorInterceptor } from 'nestjs-pino';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.setGlobalPrefix('api');
+  const options = new DocumentBuilder()
+    .setTitle('Nest Skeleton')
+    .setDescription('Nest Skeleton App ')
+    .setVersion((process.env.VERSION as string) || '1.0')
+    .addBearerAuth()
+    .build();
+
+  if (process.env.NODE_ENV === 'development') {
+    const document = SwaggerModule.createDocument(app, options);
+    SwaggerModule.setup('api/docs', app, document);
+  }
+
+  //setup logger
+  app.useLogger(app.get(Logger));
+  app.useGlobalInterceptors(new LoggerErrorInterceptor());
+
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
