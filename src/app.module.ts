c79e8125fd1be5a6a99c2677d8c45c3d21228b6e
regadior <regadior@gmail.com>
2024-05-06 21:47:17 +0200

    feat(logs): added logs save interval

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,43 +4,50 @@ import { APP_FILTER } from '@nestjs/core';
 import { LoggerModule } from 'nestjs-pino';
 // eslint-disable-next-line unicorn/import-style
 import { resolve } from 'path';
+import * as rfs from 'rotating-file-stream';
 
 import { PrismaErrorExceptionFilter } from '@presentation/exception-filter/prisma-error-exception.filter';
 
 import { UserModule } from './presentation/modules/user.module';
 
 const isProduction = process.env.TARGET_ENV === 'prod';
-const pinoFile = {
-  target: 'pino/file',
-  options: {
-    destination: resolve(
-      __dirname,
-      '..',
-      'logs',
-      `app-log-${new Date().toISOString().slice(0, 10)}.log`,
-    ),
-    prettyPrint: true,
-    rotate: {
-      interval: '1d',
-      compress: true,
-      keepFileExt: true,
-    },
-  },
+
+const customFileName = (time: any) => {
+  if (time === null) {
+    time = '';
+    return `app-log${time}.log`;
+  } else {
+    const date = new Date(time).toISOString().slice(0, 16);
+    return `app-log-${date}.log`;
+  }
 };
 const loggerConfig = {
   pinoHttp: {
     redact: isProduction ? ['req.headers.authorization'] : undefined,
-    transport: {
-      targets: [
-        ...(isProduction ? [pinoFile] : [{ target: 'pino-pretty' }, pinoFile]),
-      ],
-    },
+    ...(isProduction
+      ? {
+          stream: rfs.createStream(customFileName, {
+            interval: String(process.env.INTERVAL_LOGS) || '1d',
+            path: resolve(__dirname, '..', 'logs'),
+            intervalBoundary: true,
+            intervalUTC: true,
+            mode: 0o777,
+            maxFiles: 30,
+          }),
+        }
+      : {
+          transport: {
+            target: 'pino-pretty',
+          },
+        }),
   },
 };
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     LoggerModule.forRoot(loggerConfig),
     UserModule,
   ],
