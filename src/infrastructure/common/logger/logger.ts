13037d2454fd26fd878e78b66972aa8eee755fc1
regadior <regadior@gmail.com>
2025-05-02 11:25:25 +0200

    feat(logger): added winston logger

---
+++ src/infrastructure/common/logger/logger.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@nestjs/common';
+import * as winston from 'winston';
+import * as DailyRotateFile from 'winston-daily-rotate-file';
+
+@Injectable()
+export class LoggerService {
+  private logger: winston.Logger;
+
+  constructor() {
+    this.logger = winston.createLogger({
+      level: 'info',
+      format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.printf(({ timestamp, level, message }) => {
+          return `[${timestamp}] ${level.toUpperCase()}: ${message}`;
+        }),
+      ),
+      transports: [
+        new winston.transports.Console(),
+        new DailyRotateFile({
+          filename: 'logs/application-%DATE%.log',
+          datePattern: 'YYYY-MM-DD',
+          zippedArchive: true,
+          maxSize: '20m',
+          maxFiles: '7d',
+        }),
+      ],
+    });
+  }
+
+  log(message: string) {
+    this.logger.info(message);
+  }
+
+  error(message: string) {
+    this.logger.error(message);
+  }
+
+  warn(message: string) {
+    this.logger.warn(message);
+  }
+
+  debug(message: string) {
+    this.logger.debug(message);
+  }
+}
