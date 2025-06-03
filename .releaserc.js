44d57f72721ebadbb715eb51417a9d99eeff6378
regadior <regadior@gmail.com>
2025-06-03 22:54:02 +0200

    feat(release): added semantic-release to do automatic releases and changelog

---
+++ .releaserc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  branches: ['main', { name: 'develop', prerelease: 'beta' }],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
+    [
+      '@semantic-release/git',
+      {
+        assets: ['CHANGELOG.md', 'package.json'],
+        message: 'chore(release): ${nextRelease.version} [skip ci]',
+      },
+    ],
+    '@semantic-release/github',
+  ],
+};
