3d06f3d8bfc7dfe615ff09418073e68e10c3468f
regadior <regadior@gmail.com>
2025-06-03 23:10:32 +0200

    fix(release): removed develop because is a protected branch

--- .releaserc.js
+++ .releaserc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  branches: ['main', { name: 'develop', prerelease: 'beta' }],
+  branches: ['main'],
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
